b897938bb59a8e3d6c4a1815566816795ffe6d4c
Aranza Mena <aranzamc3@gmail.com>
2025-09-04 15:51:12 -0400

    Versión final
    Se suben modificaciones con versión final de amigo secreto

--- app.js
+++ app.js
@@ -1,1 +1,43 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+//función para agregar amigos a la lista
+function agregarAmigo(){
+    let amigoSecreto = document.getElementById("amigo").value;
+    if(amigoSecreto == ""){
+        alert("Por favor, ingresa un nombre");
+    }else{
+        amigos.push(amigoSecreto);
+        document.getElementById("amigo").value = ""; // Limpia el campo de entrada
+    }
+}
+
+//función para actualizar la lista de amigos en el DOM
+function actualizarLista(){
+    let lista = document.getElementById("listaAmigos");
+    lista.innerHTML = "";
+    for(let i = 0; i < amigos.length; i++){
+        let li = document.createElement("li");
+        li.textContent = amigos[i];
+        lista.appendChild(li);
+    }
+}
+
+
+//función para sortear un amigo secreto
+function sortearAmigo(){
+    let resultado = document.getElementById("resultado");
+    if (amigos.length === 0) {
+        resultado.innerHTML = "No hay amigos para sortear.";
+        return null; 
+    }
+    let indice = Math.floor(Math.random() * amigos.length);
+    let nombreAleatorio = amigos[indice]; 
+    resultado.innerHTML = `El amigo secreto es: ${nombreAleatorio}`;
+    return nombreAleatorio;
+}
+
+// Función que combina agregarAmigo y actualizarLista
+function agregarActualizar(){
+    agregarAmigo();
+    actualizarLista();
+}
